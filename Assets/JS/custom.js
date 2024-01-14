c10bdcd61a365baa5c94dc8dcdc0ce53cc7322d4
fire8327 <114702513+fire8327@users.noreply.github.com>
2024-01-14 18:41:25 +0300

    create reviews

--- Assets/JS/custom.js
+++ Assets/JS/custom.js
@@ -1,1 +1,10 @@
 /*jshint esnext: true */
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 2,
+    spaceBetween: 10,
+});
